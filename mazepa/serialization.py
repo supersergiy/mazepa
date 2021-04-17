49929a6115aa36d8b053384baf4c6ae8f60ead69
Sergiy Popovych <sergiy.popovich@gmail.com>
2021-04-17 08:49:19 -0700

    tests

--- mazepa/serialization.py
+++ mazepa/serialization.py
@@ -4,12 +4,15 @@ import zlib
 import objectscriber as scriber
 
 def serialize(obj):
-    '''data = pickle.dumps(obj, protocol=4)
-    data_comp = codecs.encode(zlib.compress(data), "base64")
-    import pdb; pdb.set_trace()
-    scirbed = scriber.serialize(obj)'''
+    pickled = pickle.dumps(obj, protocol=4)
+    pickled_comp = codecs.encode(zlib.compress(pickled), "base64")
     scribed = scriber.serialize(obj)
     scribed_comp = zlib.compress(scribed.encode())
+    print (f"pickled: {len(pickled)}")
+    print (f"pickled comp: {len(pickled_comp)}")
+    print (f"scribed: {len(scribed)}")
+    print (f"scribed comp: {len(scribed_comp)}")
+    import pdb; pdb.set_trace()
     return codecs.encode(scribed_comp, "base64").decode()
 
 def deserialize(s):
