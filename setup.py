19e51dccb7209e7ca0526d5ac5e22f6e9fbb487c
Sergiy Popovych <sergiy.popovich@gmail.com>
2022-06-02 19:06:24 +0000

    v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mazepa",
-    version="0.1.2",
+    version="0.1.3",
     author="Sergiy Popovych",
     author_email="sergiy.popovich@gmail.com",
     description="A tool for efficient scheduling of independent tasks to remote workers",
